060ec95a5b34755fd465f1371c1a681c01c6de01
Evelyn <evelynarrudasilva@gmail.com>
2021-11-03 16:31:05 -0300

    @evelynarruda [Task: Login - 03.11.21]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { EntrarComponent } from './entrar/entrar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntrarComponent
   ],
   imports: [
     BrowserModule,
