305f68211862d335211315498ff7285ceef6bd0a
Lucas Reis <88865748+therealkaslow@users.noreply.github.com>
2021-11-03 23:50:19 -0300

    @therealkaslow [adicionado a tela de sobre nós porém ainda imcompleta]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SobreNosComponent
   ],
   imports: [
     BrowserModule,
