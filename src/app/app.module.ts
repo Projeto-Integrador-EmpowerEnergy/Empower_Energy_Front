b7e31017787f77b11fc06fc0be5dae23ca311315
Lucas Reis <88865748+therealkaslow@users.noreply.github.com>
2021-11-09 16:29:11 -0300

    @therealkaslow [adicionado estilização da pagina sobre nós]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { NavbarComponent } from './navbar/navbar.component';
 import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
+import { LandingComponent } from './landing/landing.component';
+
+import { EntrarComponent } from './entrar/entrar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SobreNosComponent
+    SobreNosComponent,
+    LandingComponent,
+    EntrarComponent
   ],
   imports: [
     BrowserModule,
