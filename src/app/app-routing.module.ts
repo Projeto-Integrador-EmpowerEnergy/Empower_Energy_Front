42b96f845ca778bd0580d75ceffd68e794cef4d5
Lucas Reis <88865748+therealkaslow@users.noreply.github.com>
2021-11-12 15:06:05 -0300

    finalizando a estilização da pag sobre nós

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { LandingComponent } from './landing/landing.component';
+import { SobreNosComponent } from './sobre-nos/sobre-nos.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'landing', pathMatch: 'full'},
   {path: 'entrar', component: EntrarComponent},
   {path: 'cadastrar', component: CadastrarComponent},
   {path: 'inicio', component: InicioComponent},
-  {path: 'landing', component: LandingComponent}
+  {path: 'landing', component: LandingComponent},
+  {path: 'sobrenos', component: SobreNosComponent}
 ];
 
 @NgModule({
