8c9cab8eb18e6337425ad0670cb4469195925a65
caiqueb05 <caique.bezerra@gmail.com>
2021-11-19 09:04:00 -0300

    @caiqueb05 [Task: Correçoes gerais e CRUD de usuario, Metodo put 19.11.2021]

--- src/app/perfil/perfil.component.ts
+++ src/app/perfil/perfil.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { environment } from 'src/environments/environment.prod';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from '../service/auth.service';
+import { User } from '../model/User';
 
 @Component({
   selector: 'app-perfil',
@@ -10,13 +12,71 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  nome = environment.nome
+  sobrenome = environment.sobrenome
+  foto = environment.foto
+  idUser = environment.id
+  id = environment.id
+  /* idUser: number */
+
+  user: User = new User()
+  listaUsuarios: User[]
+  confirmarSenha: string
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private authService: AuthService
+    ) { }
 
   ngOnInit() {
+
+    window.scroll(0,0)
+
     if(environment.token == ''){
       // alert('Sua sessão expirou, faça o login novamente!')
       this.router.navigate(['entrar'])
     }
+
+    /* this.idUser = this.route.snapshot.params['id'] */
+    this.findByIdUser(this.idUser)
+
+    this.authService.refreshToken()
+
+    console.log(this.sobrenome)
+    console.log(this.nome)
+  }
+
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  editarUsuario(){
+    this.user.idUsuario = this.idUser
+
+    if(this.user.senhaUsuario != this.confirmarSenha){
+      alert('As senhas estão incorretas.')
+    } else {
+      console.log(this.user)
+      this.authService.atualizar(this.user).subscribe((resp: User) => {
+      this.user = resp
+      console.log(resp)
+      this.router.navigate(['/entrar'])
+      alert('Usuario atualizado com sucesso! Faça o login novamente')
+      environment.token = ''
+      environment.nome = ''
+      environment.foto = ''
+      environment.id = 0
+      this.router.navigate(['/entrar'])
+      })
+    }
+  }
+
+  findByIdUser(id: number){
+    console.log(this.id)
+    this.authService.getByIdUser(id).subscribe((resp: User) => {
+      this.user = resp
+    })
   }
 
 }
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -6,45 +6,57 @@ import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(
-    private http:HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
   token = {
-    headers: new HttpHeaders().set('Authorization', environment.token)
-  }
+    headers: new HttpHeaders().set('Authorization', environment.token),
+  };
 
-  refreshToken(){
+  refreshToken() {
     this.token = {
-      headers: new HttpHeaders().set('Authorization', environment.token)
+      headers: new HttpHeaders().set('Authorization', environment.token),
+    };
+  }
+
+  entrar(userLogin: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>(
+      'https://empowerenergyprojeto.herokuapp.com/api/v1/usuarios/logar',
+      userLogin
+    );
+  }
+
+  cadastrar(user: User): Observable<User> {
+    return this.http.post<User>(
+      'https://empowerenergyprojeto.herokuapp.com/api/v1/usuarios/cadastrar',
+      user
+    );
+  }
+
+  getByIdUser(id: number): Observable<User> {
+    return this.http.get<User>(
+      `https://empowerenergyprojeto.herokuapp.com/api/v1/usuarios/${id}`,
+      this.token
+    );
+  }
+
+  atualizar(user: User): Observable<User> {
+    return this.http.put<User>(
+      'https://empowerenergyprojeto.herokuapp.com/api/v1/usuarios/atualizar',
+      user,
+      this.token
+    );
+  }
+
+  logado() {
+    let ok = false;
+
+    if (environment.token != '') {
+      ok = true;
     }
+
+    return ok;
   }
-
- entrar(userLogin: UserLogin): Observable<UserLogin>{
-   return this.http.post<UserLogin>('https://empowerenergyprojeto.herokuapp.com/api/v1/usuarios/logar', userLogin)
- }
-
- cadastrar(user: User): Observable<User>{
-    return this.http.post<User>('https://empowerenergyprojeto.herokuapp.com/api/v1/usuarios/cadastrar', user)
 }
-
-getByIdUser(id: number): Observable<User>{
-  return this.http.get<User>(`https://empowerenergyprojeto.herokuapp.com/api/v1/usuarios/${id}`, this.token)
-}
-
-logado(){
-  let ok = false
-
-  if(environment.token != ''){
-    ok = true
-  }
-
-  return ok
-}
-
-}
-
