4fd825c68daf8418260235cda72e590f6b2b9552
Leonardo <lleonardo.azevedo@gmail.com>
2021-11-23 17:59:22 -0300

    @lleonardoazevedo[task: menu lateral esquerdo com foto -23.11.2021]

--- src/app/menu-lat-esq/menu-lat-esq.component.ts
+++ src/app/menu-lat-esq/menu-lat-esq.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { AuthService } from '../service/auth.service';
+import { User } from '../model/User';
 
 @Component({
   selector: 'app-menu-lat-esq',
@@ -9,13 +11,29 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class MenuLatEsqComponent implements OnInit {
 
+  nome = environment.nome
+  sobrenome = environment.sobrenome
+  foto = environment.foto
+  idUser: number
+  id = environment.id
+
+  user: User = new User()
+
   constructor( 
-    private router: Router
+    private router: Router,
+    private authService: AuthService
     ) { }
 
   ngOnInit() {
     if(environment.token == ''){
       this.router.navigate(['entrar'])
+    }
+
+      this.authService.refreshToken()
   }
-}
+  findByIdUser(){
+    this.authService.getByIdUser(this.id).subscribe((resp: User) => {
+      this.user = resp
+    })
+  }
 }
